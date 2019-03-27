1db9692f4d3e0b1708c2d46a60e66d6097b05496
Daiju Nakayama <42.daiju@gmail.com>
2019-03-28 00:15:07 +0900

    elm-uiの導入

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,15 @@
 module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
 
 import Browser
+import Element exposing (Element, layout)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Input as Input
 import File exposing (File)
 import File.Select as Select
 import Html exposing (Html)
-import Html.Attributes as Attr
-import Html.Events exposing (onClick)
-import Slider
 import Task
-import Http
+
 
 
 -- MAIN
@@ -28,10 +29,20 @@ main =
 -- MODEL
 
 
+type alias SliderModel =
+    { value : Float
+    , min : Float
+    , max : Float
+    , step : Float
+    , name : String
+    }
+
+
 type alias Model =
     { img_file : Maybe File
     , img_url : Maybe String
-    , widthSlider : Slider.Model
+    , widthSlider : SliderModel
+    , aSlider : SliderModel
     }
 
 
@@ -39,7 +50,8 @@ init : () -> ( Model, Cmd Msg )
 init _ =
     ( { img_file = Nothing
       , img_url = Nothing
-      , widthSlider = Slider.Model 100 10 800 1
+      , widthSlider = SliderModel 100 10 800 1 "width"
+      , aSlider = SliderModel 0.5 0.0 1.0 0.01 "a"
       }
     , Cmd.none
     )
@@ -53,8 +65,13 @@ type Msg
     = ImageRequested
     | ImageSelected File
     | ImageLoaded String
-    | SliderMsg Slider.Msg
-    | Submit
+    | WidthSliderChange Float
+    | ASliderChange Float
+
+
+updateSlider : Float -> SliderModel -> SliderModel
+updateSlider value model =
+    { model | value = value }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -75,54 +92,76 @@ update msg model =
             , Cmd.none
             )
 
-        SliderMsg slider_msg ->
-            ( { model | widthSlider = Slider.update slider_msg model.widthSlider }
+        WidthSliderChange value ->
+            ( { model | widthSlider = updateSlider value model.widthSlider }
             , Cmd.none
             )
 
-        Submit ->
-            (model, )
+        ASliderChange value ->
+            ( { model | aSlider = updateSlider value model.aSlider }
+            , Cmd.none
+            )
+
+
 
 -- VIEW
 
 
 view : Model -> Html Msg
 view model =
-    case model.img_url of
-        Nothing ->
-            Html.div []
-                [ viewImageLoadButton
-                , viewSlider "width" model.widthSlider
-                ]
+    let
+        body =
+            case model.img_url of
+                Nothing ->
+                    Element.column [ Element.padding 10 ]
+                        [ viewImageLoadButton
+                        , viewSlider WidthSliderChange model.widthSlider
+                        , viewSlider ASliderChange model.aSlider
+                        ]
 
-        Just content ->
-            Html.div []
-                [ viewImageLoadButton
-                , viewSlider "width" model.widthSlider
-                , Html.div []
-                    [ Html.img [ Attr.src content, Attr.width (round model.widthSlider.value) ] []
-                    ]
-                ]
+                Just content ->
+                    Element.column [ Element.padding 10 ]
+                        [ viewImageLoadButton
+                        , viewSlider WidthSliderChange model.widthSlider
+                        , viewSlider ASliderChange model.aSlider
+                        , Element.image
+                            [ Element.width (model.widthSlider.value |> round |> Element.px) ]
+                            { src = content, description = "画像" }
+                        ]
+    in
+    layout [] body
 
 
-viewImageLoadButton : Html Msg
+viewImageLoadButton : Element Msg
 viewImageLoadButton =
-    Html.div []
-        [ Html.button [ onClick ImageRequested ] [ Html.text "Load Image" ] ]
+    Input.button [] { onPress = Just ImageRequested, label = Element.text "load image" }
 
 
-viewSubmitButton : Html Msg
-viewSubmitButton =
-    Html.div []
-        [ Html.button [ onClick Submit ] [ Html.text "convert" ] ]
-
-
-viewSlider : String -> Slider.Model -> Html Msg
-viewSlider name model =
-    Html.div []
-        [ Html.text name
-        , Html.map SliderMsg (Slider.view [ Attr.name name ] model)
-        , Html.text (model.value |> String.fromFloat)
+viewSlider : (Float -> Msg) -> SliderModel -> Element Msg
+viewSlider onChange model =
+    Element.row []
+        [ Input.slider
+            [ Element.width (Element.px 150)
+            , Element.behindContent
+                (Element.el
+                    [ Element.width Element.fill
+                    , Element.height (Element.px 5)
+                    , Element.centerY
+                    , Background.color (Element.rgb255 120 120 120)
+                    , Border.rounded 3
+                    ]
+                    Element.none
+                )
+            ]
+            { onChange = onChange
+            , label = Input.labelLeft [] (Element.text model.name)
+            , min = model.min
+            , max = model.max
+            , value = model.value
+            , thumb = Input.defaultThumb
+            , step = Just model.step
+            }
+        , Element.text (String.fromFloat model.value)
         ]
 
 
